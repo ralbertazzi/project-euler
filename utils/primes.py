26d4eebd06aad225d55242c39bcf11fc409e21c1
Riccardo Albertazzi <piano.vergato@gmail.com>
2018-02-12 00:29:00 +0100

    Primes with sieve tests on half the numbers

--- utils/primes.py
+++ utils/primes.py
@@ -8,7 +8,7 @@ def primes_sieve(max_num):
     sieve[1] = True
     sieve[2+2::2] = True
     
-    for n in xrange(3, max_num, 2):
+    for n in xrange(3, max_num // 2, 2):
         if sieve[n] == False:
             sieve[n+n::n] = True
             
